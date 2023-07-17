ca21c4b826bc75cc3f5287e8ed708f51f9153a64
Omkar Mhaske <omkarmhaske45@gmail.com>
2023-07-17 07:36:29 +0530

    ToDo Manager using Spring Mvc

---
+++ src/main/java/com/l/MyListener.java
@@ -0,0 +1,26 @@
+package com.l;
+
+import java.util.*;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import com.entities.*;
+
+public class MyListener implements ServletContextListener {
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		
+		
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		System.out.println("context created.....");
+		List<Todo> list = new ArrayList<Todo>();
+		ServletContext context = sce.getServletContext();
+		context.setAttribute("list", list);
+	}
+
+}
